fc198ec04b03f2ce07380828b8de324cc1c7595e
Jason <diasjason7@gmail.com>
2020-07-27 21:11:55 -0700

    Fixed Handler scope

--- MicroservicesTemplateAPI/Application/Contact/Queries/GetContactsQuery.cs
+++ MicroservicesTemplateAPI/Application/Contact/Queries/GetContactsQuery.cs
@@ -9,24 +9,25 @@ namespace MicroservicesTemplateAPI.Application.Contact.Queries
 {
     public class GetContactsQuery : IRequest<List<ContactVm>>
     {
-        public class GetContactsQueryHandler : IRequestHandler<GetContactsQuery, List<ContactVm>>
+    }
+
+    public class GetContactsQueryHandler : IRequestHandler<GetContactsQuery, List<ContactVm>>
+    {
+        private readonly IContactService _contactService;
+        private readonly IMapper _mapper;
+
+        public GetContactsQueryHandler(IContactService contactService, IMapper mapper)
         {
-            private readonly IContactService _contactService;
-            private readonly IMapper _mapper;
+            _contactService = contactService;
+            _mapper = mapper;
+        }
 
-            public GetContactsQueryHandler(IContactService contactService, IMapper mapper)
-            {
-                _contactService = contactService;
-                _mapper = mapper;
-            }
+        public async Task<List<ContactVm>> Handle(GetContactsQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _contactService.GetContactsAsync();
+            var viewModel = _mapper.Map<List<ContactVm>>(response);
 
-            public async Task<List<ContactVm>> Handle(GetContactsQuery request, CancellationToken cancellationToken)
-            {
-                var response = await _contactService.GetContactsAsync();
-                var viewModel = _mapper.Map<List<ContactVm>>(response);
-
-                return await Task.FromResult(viewModel);
-            }
+            return await Task.FromResult(viewModel);
         }
     }
 }
