151225c6f9234b094f041e48c25ae0030f96569e
Jason <diasjason7@gmail.com>
2020-08-03 11:41:08 -0700

    Added Auditable Entity

---
+++ MicroservicesTemplate.Common/Entities/AuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicroservicesTemplate.Common.Entities
+{
+    public abstract class AuditableEntity
+    {
+        public int CreatedBy { get; set; }
+        public DateTime Created { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+    }
+}
