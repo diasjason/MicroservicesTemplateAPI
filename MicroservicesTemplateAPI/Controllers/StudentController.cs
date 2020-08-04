a9840b5e071c003615912daeb580ceb146f54b4b
Placida Rebello <placidarebello12@gmail.com>
2020-08-04 23:10:11 +0530

    Adding Student Controller

---
+++ MicroservicesTemplateAPI/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MicroservicesTemplate.Common.Controller;
+using MicroservicesTemplateAPI.Application.Student.Commands;
+using MicroservicesTemplateAPI.Application.Student.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroservicesTemplateAPI.Controllers
+{
+
+    public class StudentController : ApiController
+    {
+        [HttpGet]
+        public async Task<List<StudentVm>> Get()
+        {
+            return await Mediator.Send(new GetStudentsQuery());
+        }
+
+        [HttpGet("{email}")]
+        public async Task<StudentVm> Get(string email)
+        {
+            return await Mediator.Send(new GetStudentByEmailQuery { Email = email });
+        }
+
+        [HttpPost]
+        public async Task<bool> Post([FromBody] CreateStudentCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+
+        [HttpPut]
+        public async Task<bool> Update(UpdateStudentCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
